2211cdcab0863c4154c8709806cd06983ccdc461
wook <mdir2kor@gmail.com>
2024-07-04 16:44:03 +0900

    spring retry 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ configurations {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.retry:spring-retry")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/org/woogiekim/shortener/domain/shorten/application/service/ShortenService.kt
+++ src/main/kotlin/org/woogiekim/shortener/domain/shorten/application/service/ShortenService.kt
@@ -1,6 +1,7 @@
 package org.woogiekim.shortener.domain.shorten.application.service
 
 import org.springframework.dao.DuplicateKeyException
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 import org.woogiekim.shortener.domain.shorten.application.usecase.ShortenCreator
 import org.woogiekim.shortener.domain.shorten.application.usecase.ShortenCreatorCommand
@@ -12,16 +13,11 @@ import org.woogiekim.shortener.domain.shorten.domain.ShortenRepository
 class ShortenService(
     private val shortenRepository: ShortenRepository
 ) : ShortenCreator {
+
+    @Retryable(retryFor = [DuplicateKeyException::class])
     override fun create(command: ShortenCreatorCommand): Shorten {
-        repeat(3) {
-            try {
-                val shorten = with(command) { createShorten(codeGenerator, originUrl) }
+        val shorten = with(command) { createShorten(codeGenerator, originUrl) }
 
-                return shortenRepository.save(shorten)
-            } catch (ignore: DuplicateKeyException) {
-            }
-        }
-
-        throw RuntimeException()
+        return shortenRepository.save(shorten)
     }
 }
